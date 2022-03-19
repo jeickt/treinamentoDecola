0c78d31168fc8df837791730717748e02a590870
luizfelipetorres <fellipe116@gmail.com>
2022-03-18 22:06:56 -0300

    Alterações em MyApp

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -3,16 +3,22 @@
     <v-main>
       <v-container>
           <router-view></router-view>
+          <ListaPaises/>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import ListaPaises from './views/ListaPaises.vue' 
+
 export default {
   name: "App",
   data: () => ({
     //
   }),
+  components:{
+    ListaPaises
+  }
 };
 </script>
